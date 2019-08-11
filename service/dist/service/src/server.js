993dc262f952b3ce0473c36ab7f5ced14b286e7d
astjossem <tjossema@oregonstate.edu>
2019-08-10 21:54:48 -0600

    script error

--- service/dist/service/src/server.js
+++ service/dist/service/src/server.js
@@ -5,14 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const responseCodes_1 = require("../../datamodel/responseCodes");
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default());
 const port = 8000; // default port to listen
-// define a route handler for the default home page
 app.get("/messages", (req, res) => {
-    return res.status(responseCodes_1.ResponseCodes.Ok).json('Hello world!');
+    res.status(responseCodes_1.ResponseCodes.Ok).json('Hello world!');
 });
 // start the Express server
 app.listen(port, () => {
-    console.log(`server started at https://localhost:${port}`);
+    console.log(`server started at localhost:${port}`);
 });
 //# sourceMappingURL=server.js.map
--- service/src/server.ts
+++ service/src/server.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import { ResponseCodes } from '../../datamodel/responseCodes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors()); 
 const port = 8000; // default port to listen
 
-// define a route handler for the default home page
 app.get( "/messages", (req, res) => {
-    return res.status(ResponseCodes.Ok).json( 'Hello world!' );
-} );
+    res.status(ResponseCodes.Ok).json( 'Hello world!' );
+});
 
 // start the Express server
 app.listen( port, () => {
-    console.log( `server started at https://localhost:${ port }` );
+    console.log( `server started at localhost:${ port }` );
 } );
